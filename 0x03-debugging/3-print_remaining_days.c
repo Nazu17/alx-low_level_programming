49c23994fcfcb80ad92ab26d6990a81713ff7e78
Nazrawi Asrat <nasrat2676@gmail.com>
2023-06-22 12:48:40 -0700

    Like, comment, subscribe

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -18,7 +18,6 @@ void print_remaining_days(int month, int day, int year)
 	{
 	day++;
 	}
-
 	printf("Day of the year: %d\n", day);
 	printf("Remaining days: %d\n", 366 - day);
 	}
@@ -33,6 +32,6 @@ void print_remaining_days(int month, int day, int year)
 	printf("Day of the year: %d\n", day);
 	printf("Remaining days: %d\n", 365 - day);
 	}
-	}
 }
 
+#endif
