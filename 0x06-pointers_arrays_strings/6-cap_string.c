5ab374190731f3f4ca65dbfea4fb3fb6db664e5c
Nazrawi Asrat <nasrat2676@gmail.com>
2023-06-29 18:17:39 -0700

    Mozart composed his music not for the elite, but for everybody

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,7 +14,7 @@ char *cap_string(char *s)
 /* scan through string */
 	string_count = 0;
 	while (s[string_count] != '\0')
-	/* if next character after count is char , capitalise it */
+	/* if next character after count is char, capitalise it */
 		if (s[0] >= 97 && s[0] <= 122)
 		{
 			s[0] = s[0] - 32;
@@ -25,7 +25,7 @@ if (s[string_count] == ' ' || s[string_count] == '\t' || s[string_count] == '\n'
 || s[string_count] == '"' || s[string_count] == '(' || s[string_count] == ')'
 || s[string_count] == '(' || s[string_count] == ')')
 		{
-if (s[string_count + 1] >= 97 && s[string_count + 1] <= 122)
+			if (s[string_count + 1] >= 97 && s[string_count + 1] <= 122)
 			{
 				s[string_count + 1] = s[string_count + 1] - 32;
 			}
