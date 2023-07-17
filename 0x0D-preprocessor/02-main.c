10be54bb179b94f6fc456f08d095c4287719e97b
Nazrawi Asrat <nasrat2676@gmail.com>
2023-07-17 04:30:58 -0700

    File name

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -7,9 +7,9 @@
  * Return: always 0.
  */
 
-int main(void)
+int main(void);
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 
 	return (0);
 }
--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -7,7 +7,7 @@
  * Return: always 0.
  */
 
-int main(void);
+int main(void)
 {
 	printf("%s\n", __FILE__);
 
