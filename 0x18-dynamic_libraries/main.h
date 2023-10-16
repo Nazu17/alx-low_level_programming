eefad3eb3e4734f8dd73b7a455c98a5d2679cdcc
Nazrawi Asrat <nasrat2676@gmail.com>
2023-10-16 12:18:34 -0700

    A library is not a luxury but one of the necessities of life

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,11 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: MAIN_H
- * Description: Header file with all function declarations
- * for this project's tasks.
- */
+#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 int _islower(int c);
